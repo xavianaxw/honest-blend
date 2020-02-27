01c93cc8079d801a6b9ee3d32adecd77d780af0e
Xavian Ang <xavianaxw@gmail.com>
2020-02-27 12:22:00 +1100

    add conditional tasks checking

--- gulpfile.babel.js/index.js
+++ gulpfile.babel.js/index.js
@@ -1,12 +1,12 @@
 import { series, watch, parallel } from 'gulp';
 
 // Configuration files
-import paths from './config/paths';
-import tasks from './config/tasks';
+import pathsConfig from './config/paths';
+import tasksConfig from './config/tasks';
 
 // Globally expose config objects
-global.PATHS = paths;
-global.TASKS = tasks;
+global.PATHS = pathsConfig;
+global.TASKS = tasksConfig;
 
 // Tasks
 import { serve, reload } from './tasks/browser-sync';
@@ -24,26 +24,43 @@ import revTasks from './tasks/rev';
 // Helpers
 import pathBuilder from './helpers/path-builder';
 
-
 // Fetch arguments from CLI
 var argv = require('minimist')(process.argv.slice(2));
-
 global.isProduction = argv.production;
+
+// Fetch tasks with configuration only
+const tasks = [];
+
+if (tasksConfig.stylesheets) tasks.push(styles);
+if (tasksConfig.javascripts) tasks.push(scripts);
+if (tasksConfig.templates) tasks.push(templates);
+if (tasksConfig.images) tasks.push(images);
+if (tasksConfig.icons) tasks.push(icons);
+if (tasksConfig.fonts) tasks.push(fonts);
+if (tasksConfig.static) tasks.push(staticFiles);
+
+console.log(tasks);
 
 // Commands
 // gulp
 export default series(
   cleanAll,
-  parallel(styles, templates, scripts, images, icons, fonts, staticFiles),
+  // parallel(styles, templates, scripts, images, icons, fonts, staticFiles),
+  parallel(...tasks),
   serve,
   function watcher() {
-    watch(pathBuilder(PATHS.src, PATHS.stylesheets.src, `**/*.{${TASKS.stylesheets.extensions}}`), series(cleanStyles, styles));
-    watch(pathBuilder(PATHS.src, PATHS.templates.src, `**/*.{${TASKS.templates.extensions}}`), series(cleanTemplates, templates, reload));
-    watch(pathBuilder(PATHS.src, PATHS.javascripts.src, `**/*.{${TASKS.javascripts.extensions}}`), series(scripts, reload));
-    watch(pathBuilder(PATHS.src, PATHS.images.src, `**/*.{${TASKS.images.extensions}}`), series(cleanImages, images, reload));
-    watch(pathBuilder(PATHS.src, PATHS.icons.src, `**/*.svg`), series(cleanIcons, icons, reload));
-    watch(pathBuilder(PATHS.src, PATHS.fonts.src, `**/*.{${TASKS.fonts.extensions}}`), series(cleanFonts, fonts, reload));
-    watch(pathBuilder(PATHS.src, PATHS.static.src, `**/*`), series(staticFiles, reload));
+    if (tasksConfig.stylesheets)
+      watch(
+        pathBuilder(PATHS.src, PATHS.stylesheets.src, `**/*.{${TASKS.stylesheets.extensions}}`), 
+        series(cleanStyles, styles)
+      );
+      
+    // watch(pathBuilder(PATHS.src, PATHS.templates.src, `**/*.{${TASKS.templates.extensions}}`), series(cleanTemplates, templates, reload));
+    // watch(pathBuilder(PATHS.src, PATHS.javascripts.src, `**/*.{${TASKS.javascripts.extensions}}`), series(scripts, reload));
+    // watch(pathBuilder(PATHS.src, PATHS.images.src, `**/*.{${TASKS.images.extensions}}`), series(cleanImages, images, reload));
+    // watch(pathBuilder(PATHS.src, PATHS.icons.src, `**/*.svg`), series(cleanIcons, icons, reload));
+    // watch(pathBuilder(PATHS.src, PATHS.fonts.src, `**/*.{${TASKS.fonts.extensions}}`), series(cleanFonts, fonts, reload));
+    // watch(pathBuilder(PATHS.src, PATHS.static.src, `**/*`), series(staticFiles, reload));
   }
 )
 
