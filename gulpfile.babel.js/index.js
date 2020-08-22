208e00ad3ab17c262fe9437689b044b54911dcdf
Xavian Ang <xavianaxw@gmail.com>
2020-08-22 15:21:49 +1000

    fix gulpfile index file

--- gulpfile.babel.js/index.js
+++ gulpfile.babel.js/index.js
@@ -1,20 +1,15 @@
 import { series, watch, parallel } from 'gulp';
 
-// Configuration files
-import pathsConfig from './config/paths';
-import tasksConfig from './config/tasks';
-
 // Tasks
 import { serve, reload } from './tasks/browser-sync';
 import { cleanAll, cleanStyles, cleanTemplates, cleanImages, cleanFonts, cleanIcons } from './tasks/clean';
 import { styles } from './tasks/stylesheets';
 import { templates } from './tasks/templates';
-// import { scripts } from './tasks/scripts';
+import { scripts } from './tasks/scripts';
 import { images } from './tasks/images';
 import { icons } from './tasks/icons';
 import { fonts } from './tasks/fonts';
 import { staticFiles } from './tasks/static';
-// import { revAssets, revUpdateReferences, revCss, revUpdateHtml } from './tasks/rev';
 import revTasks from './tasks/rev';
 
 // Helpers
@@ -32,7 +27,7 @@ global.isProduction = config.isProduction;
 const tasks = [];
 
 if (TASKSCONFIG.stylesheets) tasks.push(styles);
-// if (TASKSCONFIG.javascripts) tasks.push(scripts);
+if (TASKSCONFIG.javascripts) tasks.push(scripts);
 if (TASKSCONFIG.templates) tasks.push(templates);
 if (TASKSCONFIG.images) tasks.push(images);
 if (TASKSCONFIG.icons) tasks.push(icons);
@@ -49,27 +44,20 @@ export default series(
     if (TASKSCONFIG.stylesheets)
       watch(pathBuilder(PATHSCONFIG.src, PATHSCONFIG.stylesheets.src, `**/*.{${TASKSCONFIG.stylesheets.extensions}}`), series(cleanStyles, styles));
   
-    // if (TASKSCONFIG.javascripts) 
-    //   watch(pathBuilder(PATHSCONFIG.src, PATHSCONFIG.javascripts.src, `**/*.{${TASKSCONFIG.javascripts.extensions}}`), series(scripts, reload));
-
-    // if (TASKSCONFIG.javascripts) {
-    //   watch(pathBuilder(PATHSCONFIG.src, PATHSCONFIG.javascripts.src, `**/*.{${TASKSCONFIG.javascripts.extensions}}`), series(reload));
-    // }
-  
-    // if (TASKSCONFIG.templates)
-    //   watch(pathBuilder(PATHSCONFIG.src, PATHSCONFIG.templates.src, `**/*.{${TASKSCONFIG.templates.extensions}}`), series(cleanTemplates, templates, reload));
+    if (TASKSCONFIG.templates)
+      watch(pathBuilder(PATHSCONFIG.src, PATHSCONFIG.templates.src, `**/*.{${TASKSCONFIG.templates.extensions}}`), series(cleanTemplates, templates, reload));
       
-    // if (TASKSCONFIG.images)
-    //   watch(pathBuilder(PATHSCONFIG.src, PATHSCONFIG.images.src, `**/*.{${TASKSCONFIG.images.extensions}}`), series(cleanImages, images, reload));
+    if (TASKSCONFIG.images)
+      watch(pathBuilder(PATHSCONFIG.src, PATHSCONFIG.images.src, `**/*.{${TASKSCONFIG.images.extensions}}`), series(cleanImages, images, reload));
   
-    // if (TASKSCONFIG.icons)
-    //   watch(pathBuilder(PATHSCONFIG.src, PATHSCONFIG.icons.src, `**/*.svg`), series(cleanIcons, icons, reload));
+    if (TASKSCONFIG.icons)
+      watch(pathBuilder(PATHSCONFIG.src, PATHSCONFIG.icons.src, `**/*.svg`), series(cleanIcons, icons, reload));
   
-    // if (TASKSCONFIG.fonts)
-    //   watch(pathBuilder(PATHSCONFIG.src, PATHSCONFIG.fonts.src, `**/*.{${TASKSCONFIG.fonts.extensions}}`), series(cleanFonts, fonts, reload));
+    if (TASKSCONFIG.fonts)
+      watch(pathBuilder(PATHSCONFIG.src, PATHSCONFIG.fonts.src, `**/*.{${TASKSCONFIG.fonts.extensions}}`), series(cleanFonts, fonts, reload));
   
-    // if (TASKSCONFIG.staticFiles)
-    //   watch(pathBuilder(PATHSCONFIG.src, PATHSCONFIG.static.src, `**/*`), series(staticFiles, reload));
+    if (TASKSCONFIG.staticFiles)
+      watch(pathBuilder(PATHSCONFIG.src, PATHSCONFIG.static.src, `**/*`), series(staticFiles, reload));
   }
 )
 
